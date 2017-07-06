97b8456c488e1e01d79efe1f87db120fa2a163ba
Beta Version Software <betaversionsoftware@gmail.com>
2017-07-06 22:53:22 +0800

    Reorganise worker and receiver

--- worker.rb
+++ worker.rb
@@ -11,42 +11,9 @@ end
 class Message
   include Sidekiq::Worker
 
-  def perform(msg)
+  def perform(plane)
     valid_msg = %i(callsign altitude speed lat lng track squawk)
-	  
-    log = msg.split(',')
-    msg_n = log[1].to_i
-
-    icao = log[4]
-    plane = {}
-
-    plane[:last_msg] = DateTime.now
-
-    case msg_n
-    when 1
-      plane[:callsign] = log[10].strip unless log[10].empty?
-    when 2
-      plane[:altitude] = log[11].to_i unless log[11].empty?
-      plane[:speed] = log[12].to_i unless log[12].empty?
-      plane[:track] = log[13].to_i unless log[13].empty?
-      plane[:lat] = log[14].to_f unless log[14].empty?
-      plane[:lng] = log[15].to_f unless log[15].empty?
-    when 3
-      plane[:altitude] = log[11].to_i unless log[11].empty?
-      plane[:lat] = log[14].to_f unless log[14].empty?
-      plane[:lng] = log[15].to_f unless log[15].empty?
-    when 4
-      plane[:speed] = log[12].to_i unless log[12].empty?
-      plane[:track] = log[13].to_i  unless log[13].empty?
-    when 5
-      plane[:altitude] = log[11].to_i unless log[11].empty?
-    when 6
-      plane[:altitude] = log[11].to_i unless log[11].empty?
-      plane[:squawk] = log[17].to_i unless log[17].empty?
-    when 7
-      plane[:altitude] = log[11].to_i unless log[11].empty?
-    end
-
     valid = valid_msg.all? { |v| plane.key?(v) }
+    puts plane if valid
   end
 end
