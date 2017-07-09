f63fa6fa96187ce8396d498809e457866b4744a8
Beta Version Software <betaversionsoftware@gmail.com>
2017-07-09 16:34:26 +0000

    Add dc.sh

---
+++ dc.sh
@@ -0,0 +1,3 @@
+mkdir -p /opt/bin
+curl -L "https://github.com/docker/compose/releases/download/1.14.0/docker-compose-$(uname -s)-$(uname -m)" -o /opt/bin/docker-compose
+chmod +x /opt/bin/docker-compose
