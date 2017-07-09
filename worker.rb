2cd262d9d08313f57ea300e87dea0f6c52588ad5
Beta Version Software <betaversionsoftware@gmail.com>
2017-07-09 16:12:41 +0000

    Add mongo

--- worker.rb
+++ worker.rb
@@ -1,4 +1,5 @@
 require 'sidekiq'
+require 'mongo'
 
 Sidekiq.configure_client do |config|
   config.redis = { url: 'redis://redis:6379/1' }
@@ -11,9 +12,16 @@ end
 class Message
   include Sidekiq::Worker
 
+  @traffic = nil
+
+  def initialize
+    mongo_client = Mongo::Client.new([ 'mongo:27017' ], :database => 'airtraffic24')
+    @traffic = mongo_client[:traffic]
+  end
+
   def perform(plane)
     valid_msg = %w(icao callsign altitude speed lat lng track squawk)
     valid = valid_msg.all? { |v| plane.key?(v) }
-    puts plane if valid
+    @traffic.insert_one(plane) if valid
   end
 end
