c53e740d48e06ed375a2d4fafdd90734697e69f2
Beta Version Software <betaversionsoftware@gmail.com>
2017-10-19 15:58:27 +0000

    Clean up

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,8 @@
 FROM ruby
 RUN gem install \
-  sidekiq \
   redis \
   mongo \
   json \
   activesupport \
-  sinatra
-EXPOSE 1234/udp
+  sinatra \
+  bunny
--- at24.rb
+++ at24.rb
@@ -1,7 +1,30 @@
-require 'socket'
 require 'mongo'
-require_relative 'worker'
+require 'date'
+require 'json'
+require 'bunny'
 
-Socket.udp_server_loop(host='0.0.0.0', 1234) do |msg, msg_src|
-  Message.perform_async(msg)
+mongo_client = Mongo::Client.new('mongodb://mongo:27017/atr24')
+current = mongo_client[:current]
+traffic = mongo_client[:traffic]
+
+begin
+  conn = Bunny.new(hostname: "mq.airtraffic24.com")
+  conn.start
+  ch = conn.create_channel
+  q = ch.queue("traffic", durable: true)
+rescue => e
+  sleep(5)
+  retry
+end
+
+begin
+  q.subscribe(:block => true) do |delivery_info, properties, body|
+    json_plane = JSON.parse(body)
+    json_plane['last_seen'] ||= DateTime.now
+
+    traffic.insert_one(json_plane)
+    current.find_one_and_update({'icao': json_plane['icao']}, {'$set': json_plane, '$inc': {messages: 1}}, { 'upsert': true})
+  end
+rescue => e
+  logger.warn("Duplicate #{e}")
 end
